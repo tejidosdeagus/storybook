b44cf270829d02e4cdb81e8c41c405ecba1ed209
Agus <agustinabmendez@gmail.com>
2025-04-29 16:26:51 -0300

    Update blogCard

--- src/components/BlogCard/BlogCard.tsx
+++ src/components/BlogCard/BlogCard.tsx
@@ -13,59 +13,87 @@ export type BlogCardProps = {
 
 export const BlogCard = ({ image, date, title, summary, linkTo, onReadMore }: BlogCardProps) => {
   return (
-    <Card sx={{ width: 354, backgroundColor: '#D4AF80', borderRadius: 2 }}>
-      <CardMedia
-        component="img"
-        image={image}
-        alt={title}
-        sx={{ width: '100%', height: 200, objectFit: 'cover' }}
-      />
-      <CardContent sx={{ px: 2, py: 2 }}>
+    <Card
+    sx={{
+      width: 600,
+      height: 750,
+      backgroundColor: '#D4AF80',
+      borderRadius: 2,
+      overflow: 'hidden',
+      display: 'flex',
+      flexDirection: 'column',
+      paddingTop: '50px',
+    }}
+  >
+    <CardMedia
+      component="img"
+      image={image}
+      alt={title}
+      sx={{
+        width: 490,
+        height: 325,
+        objectFit: 'cover',
+        alignSelf: 'center',
+      }}
+    />
+    <CardContent
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        flexGrow: 1,
+        justifyContent: 'space-between',
+        px: 2,
+        py: 2,
+      }}
+    >
+      <div>
         <Typography
           sx={{
             fontFamily: 'Italiana',
             fontStyle: 'italic',
-            fontSize: 20,
+            fontSize: 32,
             color: '#4A4A4A',
           }}
         >
           {date}
         </Typography>
-
+  
         <Typography
           sx={{
             fontFamily: 'Playfair Display',
             fontWeight: 700,
-            fontSize: 24,
+            fontSize: 32,
             color: '#4A4A4A',
-           
           }}
         >
           {title}
         </Typography>
-
+  
         <Typography
           sx={{
             fontFamily: 'Playfair Display',
-            fontSize: 18,
+            fontSize: 32,
             color: '#D54848',
-            mt: 1.5,
             mb: 2,
             display: '-webkit-box',
-            WebkitLineClamp: 3,
+            WebkitLineClamp: 5,
             WebkitBoxOrient: 'vertical',
             overflow: 'hidden',
           }}
         >
           {summary}
         </Typography>
-        <Button
-            onClick={onReadMore}
-            variant="secondary"
-          >
-            Agregar al carrito
-          </Button>
-      </CardContent>
-    </Card>
+      </div>
+  
+      <Button
+        onClick={onReadMore}
+        variant="secondary"
+        sx={{ alignSelf: 'flex-start'}}
+      >
+        Seguir leyendo
+      </Button>
+    </CardContent>
+  </Card>
+  
   );
 };
