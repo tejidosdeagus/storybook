88b66a2a01a12c642c245f4bbfd54e911a2d259b
lautarodebenedetti <lautarodebenedetti@gmail.com>
2025-06-20 00:12:17 -0300

    fix(storybook): Updating component BlogCard

--- src/components/BlogCard/BlogCard.tsx
+++ src/components/BlogCard/BlogCard.tsx
@@ -1,99 +1,105 @@
-import { Card, CardContent, CardMedia, Typography} from '@mui/material';
-import { Button } from '../Button';
-
+import { Card, CardContent, CardMedia, Typography } from "@mui/material";
+import { Button } from "../Button";
 
 export type BlogCardProps = {
   image: string;
   date: string;
   title: string;
   summary: string;
-  linkTo?: string;
-  onReadMore: () => void;
+  onClick: () => void;
 };
 
-export const BlogCard = ({ image, date, title, summary, linkTo, onReadMore }: BlogCardProps) => {
+export const BlogCard = ({
+  image,
+  date,
+  title,
+  summary,
+  onClick,
+}: BlogCardProps) => {
   return (
     <Card
-    sx={{
-      width: 600,
-      height: 750,
-      backgroundColor: '#D4AF80',
-      borderRadius: 2,
-      overflow: 'hidden',
-      display: 'flex',
-      flexDirection: 'column',
-      paddingTop: '50px',
-    }}
-  >
-    <CardMedia
-      component="img"
-      image={image}
-      alt={title}
       sx={{
-        width: 490,
-        height: 325,
-        objectFit: 'cover',
-        alignSelf: 'center',
-      }}
-    />
-    <CardContent
-      sx={{
-        display: 'flex',
-        flexDirection: 'column',
-        flexGrow: 1,
-        justifyContent: 'space-between',
-        px: 2,
-        py: 2,
+        minWidth: 300,
+        maxWidth: 500,
+        minHeight: 600,
+        maxHeight: 750,
+        backgroundColor: "#D4AF80",
+        borderRadius: 2,
+        overflow: "hidden",
+        display: "flex",
+        flexDirection: "column",
+        paddingTop: "50px",
       }}
     >
-      <div>
-        <Typography
-          sx={{
-            fontFamily: 'Italiana',
-            fontStyle: 'italic',
-            fontSize: 32,
-            color: '#4A4A4A',
-          }}
-        >
-          {date}
-        </Typography>
-  
-        <Typography
-          sx={{
-            fontFamily: 'Playfair Display',
-            fontWeight: 700,
-            fontSize: 32,
-            color: '#4A4A4A',
-          }}
-        >
-          {title}
-        </Typography>
-  
-        <Typography
-          sx={{
-            fontFamily: 'Playfair Display',
-            fontSize: 32,
-            color: '#D54848',
-            mb: 2,
-            display: '-webkit-box',
-            WebkitLineClamp: 5,
-            WebkitBoxOrient: 'vertical',
-            overflow: 'hidden',
-          }}
-        >
-          {summary}
-        </Typography>
-      </div>
-  
-      <Button
-        onClick={onReadMore}
-        variant="secondary"
-        sx={{ alignSelf: 'flex-start'}}
+      <CardMedia
+        component="img"
+        image={image}
+        alt={title}
+        sx={{
+          minWidth: 300,
+          height: 325,
+          objectFit: "cover",
+          alignSelf: "center",
+        }}
+      />
+      <CardContent
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          flexGrow: 1,
+          justifyContent: "space-between",
+          px: 2,
+          py: 1,
+        }}
       >
-        Seguir leyendo
-      </Button>
-    </CardContent>
-  </Card>
-  
+        <div>
+          <Typography
+            sx={{
+              fontFamily: "Italiana",
+              fontStyle: "italic",
+              fontSize: 32,
+              color: "#4A4A4A",
+            }}
+          >
+            {date}
+          </Typography>
+
+          <Typography
+            sx={{
+              fontFamily: "Playfair Display",
+              fontWeight: 700,
+              fontSize: 32,
+              color: "#4A4A4A",
+            }}
+          >
+            {title}
+          </Typography>
+
+          <Typography
+            sx={{
+              fontFamily: "Playfair Display",
+              fontSize: 32,
+              color: "#D54848",
+              display: "-webkit-box",
+              WebkitLineClamp: 2,
+              WebkitBoxOrient: "vertical",
+              overflow: "hidden",
+              textOverflow: "ellipsis",
+              whiteSpace: "normal",
+            }}
+          >
+            {summary}
+          </Typography>
+        </div>
+
+        <Button
+          onClick={onClick}
+          variant="secondary"
+          sx={{ alignSelf: "flex-start" }}
+        >
+          Seguir leyendo
+        </Button>
+      </CardContent>
+    </Card>
   );
 };
--- src/stories/BlogCard.stories.tsx
+++ src/stories/BlogCard.stories.tsx
@@ -12,10 +12,10 @@ type Story = StoryObj<typeof BlogCard>;
 
 export const Default: Story = {
   args: {
-    image: "https://via.placeholder.com/354x200.png?text=Imagen+Blog",
+    image: "src/assets/img1.png",
     date: "08 de febrero, 2025",
     title: "Bolso red: tutorial gratuito",
     summary: `Este verano decidí regalarle algo especial a mi comunidad: un patrón gratuito para que más personas puedan disfrutar del tejido tanto como yo. La inspiración vino de una bolsa que había tejido hace un tiempo para una tienda de bikinis. Me preguntaron tantas veces por el patrón que supe que tenía que compartirlo.`,
-    onReadMore: () => alert("Seguir leyendo..."),
+    onClick: () => alert("Seguir leyendo..."),
   },
 };
